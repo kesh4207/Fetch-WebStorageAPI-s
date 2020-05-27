c184ce05de8f9201c4fea947338e307ade6e2454
kesh4207 <keseanarobinson1@outlook.com>
2020-05-27 14:58:35 +0100

    Adding Files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Fetch-WebStorageAPI-s
+
+# Write the HTML, CSS and JavaScript to do the following:
+
+1. Fetch an array of wall items
+
+2. Display those items on the website
+
+3. Allow users to enter their own items
+
+4. Use this JSFiddle for details
---
+++ app.js
@@ -0,0 +1,13 @@
+fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then( listItems => addElements(listItems));
+
+function addElements(lists){
+
+let currentList = document.getElementById('list');
+lists.forEach(list => {
+let x = document.createElement('li')
+x.innerText= list;
+currentList.appendChild(x);
+
+
+})
+};
