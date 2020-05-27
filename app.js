33c11afdae3bcd8b061f9308954f0568109f9a55
kesh4207 <keseanarobinson1@outlook.com>
2020-05-27 15:39:33 +0100

    allow users to enter own items

--- app.js
+++ app.js
@@ -11,18 +11,22 @@ currentList.appendChild(x);
 
 })
 }
+function addItem()
+{
+    
 
-let userItem = prompt("Enter Item: ");
-
-fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
-  method: 'POST',
-  body: JSON.stringify({message: userItem})
-}).then(function (response) {
-  if (response.status == 200) {
-    console.log("Ok!")
-  } else {console.log("Oh no!")}
-});
-let currentList = document.getElementById('list');
-let newItem = document.createElement('li')
-newItem.innerText= userItem;
-currentList.appendChild(newItem);
+        let itemToAdd = document.getElementById('adding')[0].value;
+        fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
+        method: 'POST',
+        body: JSON.stringify({message: itemToAdd})
+        }).then(function (response) {
+        if (response.status == 200) {
+            console.log("Ok!")
+        } else {console.log("Oh no!")}
+        });
+        let currentList = document.getElementById('list');
+        let newItem = document.createElement('li')
+        newItem.innerText= itemToAdd;
+        currentList.appendChild(newItem);
+        return false;
+}
